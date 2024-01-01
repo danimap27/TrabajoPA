164fd999d926d5931d4cf72f48613d60d17788cd
Daniel Martin <74870961+danimap27@users.noreply.github.com>
2024-01-01 11:00:58 +0100

    Update sist_gest_tick_sop.sql

--- sist_gest_tick_sop.sql
+++ sist_gest_tick_sop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-12-2023 a las 19:00:08
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 01-01-2024 a las 10:54:39
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,13 +29,11 @@ USE `sist_gest_tick_sop`;
 -- Estructura de tabla para la tabla `agente`
 --
 
-DROP TABLE IF EXISTS `agente`;
-CREATE TABLE IF NOT EXISTS `agente` (
-  `idAgente` int(8) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `agente` (
+  `idAgente` int(8) NOT NULL,
   `nombreAgente` varchar(255) NOT NULL,
   `apellidosAgente` varchar(255) NOT NULL,
-  `clave` text NOT NULL,
-  PRIMARY KEY (`idAgente`)
+  `clave` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -44,14 +42,12 @@ CREATE TABLE IF NOT EXISTS `agente` (
 -- Estructura de tabla para la tabla `cliente`
 --
 
-DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE IF NOT EXISTS `cliente` (
-  `idCliente` int(8) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cliente` (
+  `idCliente` int(8) NOT NULL,
   `nombreCliente` varchar(255) NOT NULL,
   `apellidoCliente` varchar(255) NOT NULL,
   `codigoPostal` varchar(5) NOT NULL,
-  `clave` text NOT NULL,
-  PRIMARY KEY (`idCliente`)
+  `clave` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -60,18 +56,96 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 -- Estructura de tabla para la tabla `ticket`
 --
 
-DROP TABLE IF EXISTS `ticket`;
-CREATE TABLE IF NOT EXISTS `ticket` (
-  `id_ticket` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ticket` (
+  `idTicket` int(5) NOT NULL,
   `nombreTicket` varchar(50) NOT NULL,
   `descripcionTicket` text NOT NULL,
   `prioridad` int(11) NOT NULL,
   `estado` int(11) NOT NULL,
   `fk_idCliente` int(8) NOT NULL,
   `fk_idAgente` int(8) NOT NULL,
-  `fechaRegistro` date NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id_ticket`)
+  `fechaRegistro` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `contrasenia_hash` varchar(255) NOT NULL,
+  `tipo` enum('cliente','administrador','agente') NOT NULL,
+  `idCorrespondiente` int(11) DEFAULT NULL
+) ;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `correo`, `contrasenia_hash`, `tipo`, `idCorrespondiente`) VALUES
+(1, 'admin@admin.com', 'admin', 'administrador', NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `agente`
+--
+ALTER TABLE `agente`
+  ADD PRIMARY KEY (`idAgente`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indices de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`idTicket`),
+  ADD KEY `fk_agente` (`fk_idAgente`),
+  ADD KEY `fk_usuario` (`fk_idCliente`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD KEY `idCorrespondiente` (`idCorrespondiente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `agente`
+--
+ALTER TABLE `agente`
+  MODIFY `idAgente` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idCliente` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `idTicket` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -83,6 +157,13 @@ CREATE TABLE IF NOT EXISTS `ticket` (
 ALTER TABLE `ticket`
   ADD CONSTRAINT `fk_agente` FOREIGN KEY (`fk_idAgente`) REFERENCES `agente` (`idAgente`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`fk_idCliente`) REFERENCES `cliente` (`idCliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idCorrespondiente`) REFERENCES `cliente` (`idCliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`idCorrespondiente`) REFERENCES `agente` (`idAgente`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
